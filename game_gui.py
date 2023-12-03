e6ad5ea4acd6ad29d2fc1622d4026768ff4ef4e2
Ian Chan <ianchan1108@hotmail.com>
2023-12-02 21:27:59 -0800

    Add boundaries, character_sprite

--- game_gui.py
+++ game_gui.py
@@ -6,41 +6,144 @@ import pygame
 
 pygame.init()
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 800
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 400
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 pygame.display.set_caption("Pikachu's Adventure!")
 
-PLAYER_WIDTH = 20
-PLAYER_HEIGHT = 20
-player = pygame.Rect((0, 0, PLAYER_WIDTH, PLAYER_HEIGHT))
+walkRight = [pygame.image.load('images/character/R1.png'), pygame.image.load('images/character/R2.png')]
+walkLeft = [pygame.image.load('images/character/L1.png'), pygame.image.load('images/character/L2.png')]
+walkUp = [pygame.image.load('images/character/U1.png'), pygame.image.load('images/character/U2.png')]
+walkDown = [pygame.image.load('images/character/D1.png'), pygame.image.load('images/character/D2.png')]
 
+charRight = pygame.image.load('images/character/Rstill.png')
+charLeft = pygame.image.load('images/character/Lstill.png')
+charUp = pygame.image.load('images/character/Ustill.png')
+charDown = pygame.image.load('images/character/Dstill.png')
+
+clock = pygame.time.Clock()
+player_width = 0
+player_column = 0
+PLAYER_WIDTH = 24
+PLAYER_HEIGHT = 24
 SPEED = 2
+left = False
+right = False
+up = False
+down = False
+walkCount = 0
+
+# Direction character is facing
+facing_left = False
+facing_right = False
+facing_up = False
+facing_down = False
+
+# character
+player = pygame.Rect((player_width, player_column, PLAYER_WIDTH, PLAYER_HEIGHT))
 
 
-run = True
-while run:
-    pygame.time.delay(5)
+def redraw_window():
+    global walkCount
+    global facing_left
+    global facing_right
+    global facing_up
+    global facing_down
+
+    # update background
     screen.fill((0, 0, 0))
 
-    pygame.draw.rect(screen, (255, 0, 0), player)
+    # create character
+    if walkCount >= 12:
+        walkCount = 0
 
-    key = pygame.key.get_pressed()
-    if (key[pygame.K_a] or key[pygame.K_LEFT]) and player[0] > SPEED:
-        player.move_ip(-SPEED, 0)
-    elif (key[pygame.K_d] or key[pygame.K_RIGHT]) and player[0] < SCREEN_WIDTH - PLAYER_WIDTH + SPEED:
-        player.move_ip(SPEED, 0)
-    elif (key[pygame.K_w] or key[pygame.K_UP]) and player[1] > SPEED:
-        player.move_ip(0, -SPEED)
-    elif (key[pygame.K_s] or key[pygame.K_DOWN]) and player[1] < SCREEN_HEIGHT - PLAYER_HEIGHT + SPEED:
-        player.move_ip(0, SPEED)
+    if left:
+        screen.blit(walkLeft[walkCount // 6], (player[0], player[1]))
+        facing_left = True
+        facing_right = facing_up = facing_down = False
+    elif right:
+        screen.blit(walkRight[walkCount // 6], (player[0], player[1]))
+        facing_right = True
+        facing_left = facing_up = facing_down = False
+    elif up:
+        screen.blit(walkUp[walkCount // 6], (player[0], player[1]))
+        facing_up = True
+        facing_left = facing_right = facing_down = False
+    elif down:
+        screen.blit(walkDown[walkCount // 6], (player[0], player[1]))
+        facing_down = True
+        facing_left = facing_right = facing_up = False
+    else:
+        if facing_right:
+            screen.blit(charRight, (player[0], player[1]))
+        elif facing_left:
+            screen.blit(charLeft, (player[0], player[1]))
+        elif facing_up:
+            screen.blit(charUp, (player[0], player[1]))
+        elif facing_down:
+            screen.blit(charDown, (player[0], player[1]))
+        else:
+            screen.blit(charRight, (player[0], player[1]))
 
+    walkCount += 1
+
+    # pygame.draw.rect(screen, (255, 0, 0), player)
+
+    pygame.display.update()
+
+
+# main_loop
+run = True
+while run:
+    clock.tick(50)
+
+    # quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
 
-    pygame.display.update()
+        # handle key events
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                left = True
+                right = up = down = False
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                right = True
+                left = up = down = False
+            elif event.key == pygame.K_w or event.key == pygame.K_UP:
+                up = True
+                left = right = down = False
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                down = True
+                left = right = up = False
+
+        elif event.type == pygame.KEYUP:
+            # reset the flags when the key is released
+            if event.key == pygame.K_a or event.key == pygame.K_LEFT:
+                left = False
+            elif event.key == pygame.K_d or event.key == pygame.K_RIGHT:
+                right = False
+            elif event.key == pygame.K_w or event.key == pygame.K_UP:
+                up = False
+            elif event.key == pygame.K_s or event.key == pygame.K_DOWN:
+                down = False
+
+    # movement keys
+    if left and player[0] > SPEED - SPEED:
+        player.move_ip(-SPEED, 0)
+        walkCount += 1
+    elif right and player[0] < SCREEN_WIDTH - PLAYER_WIDTH + SPEED:
+        player.move_ip(SPEED, 0)
+        walkCount += 1
+    elif up and player[1] > SPEED - SPEED:
+        player.move_ip(0, -SPEED)
+        walkCount += 1
+    elif down and player[1] < SCREEN_HEIGHT - PLAYER_HEIGHT + SPEED:
+        player.move_ip(0, SPEED)
+        walkCount += 1
+
+    redraw_window()
 
 pygame.quit()
