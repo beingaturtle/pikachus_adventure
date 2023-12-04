6a7c09906dfe0e525569b10445c9137079609bfe
Edro <edrogonzales@hotmail.com>
2023-12-03 21:37:07 -0800

    Rename files + add additional tests

--- tests/test_get_name.py
+++ tests/test_get_name.py
@@ -3,8 +3,8 @@ from unittest.mock import patch
 from game import get_name
 
 class TestGetName(TestCase):
-    @patch('builtins.input', return_value='Ash Ketchum')
+    @patch('builtins.input', return_value='Ash_Ketchum')
     def test_get_name_valid(self, _):
-        expected_name = 'Ash Ketchum'
+        expected_name = 'Ash_Ketchum'
         result = get_name()
         self.assertEqual(result, expected_name)
