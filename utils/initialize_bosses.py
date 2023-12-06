49efe33852295701ddecdf793599046112cec4cf
Edro <edrogonzales@hotmail.com>
2023-12-06 02:10:03 -0800

    Initialize boss function

---
+++ utils/initialize_bosses.py
@@ -0,0 +1,35 @@
+# character_info = {
+#     "name": "hi",
+#     "health": 100,
+#     "coordinates": (2, 1),
+#     "skill": "tackle",
+#     "attack_power": 10,
+#     "defense": 10,
+#     "keys": 13,
+#     "total_experience": 0,
+#     "current_boss": 0
+# }
+
+def has_enough_keys(character_info, boss_info):
+    if character_info["keys"] >= boss_info["required_key"]:
+        return True
+
+
+def initialize_bosses():
+    """
+    Initializes bosses.
+
+    :postcondition: initializes a list of bosses and their information
+    :return: list of bosses and their information
+    """
+    first_boss = {
+        "name": "Grace Hopper",
+        "health": 100,
+        "coordinates": [
+            446, 253
+        ],
+        "skill": "cross-arms",
+        "attack_power": 10,
+        "defense": 10,
+        "keys_required": 3,
+    }
