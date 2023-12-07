35ecb2795af56178bb4df65468ff5cb7ba00c4d9
Edro <edrogonzales@hotmail.com>
2023-12-07 01:18:50 -0800

    Refactor game.py for modularity and clarity

--- game.py
+++ game.py
@@ -10,247 +10,153 @@ from utils.get_name import get_name
 from utils.user_has_file import user_has_file
 from utils.generate_character_info import generate_character_info
 from utils.get_save_file import get_save_file
-from utils.initialize_bosses import initialize_bosses
 from game_gui.display_prompt import display_prompt
 from game_gui.show_intro_screen import show_intro_screen
-from game_gui.information_box import information_box
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT, GRID_SIZE, CELL_SIZE, PLAYER_WIDTH, PLAYER_HEIGHT, SPEED
+from game_gui.drawing import redraw_window
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, CELL_SIZE, PLAYER_WIDTH, PLAYER_HEIGHT, SPEED
 
-walkCount = 0
-
-left = False
-right = False
-up = False
-down = False
-
-game_run = True
-
-facing_left = False
-facing_right = False
-facing_up = False
-facing_down = False
-
-screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-player = pygame.Rect(0, 0, PLAYER_WIDTH, PLAYER_HEIGHT)
-clock = pygame.time.Clock()
-
-pygame.display.set_caption("Pikachu's Adventure!")
-
-walkRight = [
-    pygame.transform.scale(pygame.image.load('./images/character/R1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
-    pygame.transform.scale(pygame.image.load('./images/character/R2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-]
-walkLeft = [
-    pygame.transform.scale(pygame.image.load('./images/character/L1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
-    pygame.transform.scale(pygame.image.load('./images/character/L2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-]
-walkUp = [
-    pygame.transform.scale(pygame.image.load('./images/character/U1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
-    pygame.transform.scale(pygame.image.load('./images/character/U2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-]
-walkDown = [
-    pygame.transform.scale(pygame.image.load('./images/character/D1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
-    pygame.transform.scale(pygame.image.load('./images/character/D2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-]
-
-charRight = pygame.transform.scale(pygame.image.load('./images/character/Rstill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-charLeft = pygame.transform.scale(pygame.image.load('./images/character/Lstill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-charUp = pygame.transform.scale(pygame.image.load('./images/character/Ustill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-charDown = pygame.transform.scale(pygame.image.load('./images/character/Dstill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
-
-def draw_character() -> None:
+def load_character_images():
     """
-    Draw the character on the screen based on the current state and direction it is facing.
+    Load and scale character images for various movements.
 
-    :postcondition: character is drawn on the screen facing north, south, east, or west
+    :return: a tuple containing lists of Pygame surfaces for each direction
     """
-    global walkCount
-    global facing_left
-    global facing_right
-    global facing_up
-    global facing_down
+    walk_left = [
+        pygame.transform.scale(pygame.image.load('./images/character/L1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
+        pygame.transform.scale(pygame.image.load('./images/character/L2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    ]
+    walk_right = [
+        pygame.transform.scale(pygame.image.load('./images/character/R1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
+        pygame.transform.scale(pygame.image.load('./images/character/R2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    ]
+    walk_up = [
+        pygame.transform.scale(pygame.image.load('./images/character/U1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
+        pygame.transform.scale(pygame.image.load('./images/character/U2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    ]
+    walk_down = [
+        pygame.transform.scale(pygame.image.load('./images/character/D1.png'), (PLAYER_WIDTH, PLAYER_HEIGHT)),
+        pygame.transform.scale(pygame.image.load('./images/character/D2.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    ]
+    char_right = pygame.transform.scale(pygame.image.load('./images/character/Rstill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    char_left = pygame.transform.scale(pygame.image.load('./images/character/Lstill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    char_up = pygame.transform.scale(pygame.image.load('./images/character/Ustill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
+    char_down = pygame.transform.scale(pygame.image.load('./images/character/Dstill.png'), (PLAYER_WIDTH, PLAYER_HEIGHT))
 
-    # create character
-    if walkCount >= 12:
-        walkCount = 0
+    return walk_left, walk_right, walk_up, walk_down, char_right, char_left, char_up, char_down
 
-    if left:
-        screen.blit(walkLeft[walkCount // 6], (player[0], player[1]))
-        facing_left = True
-        facing_right = facing_up = facing_down = False
-    elif right:
-        screen.blit(walkRight[walkCount // 6], (player[0], player[1]))
-        facing_right = True
-        facing_left = facing_up = facing_down = False
-    elif up:
-        screen.blit(walkUp[walkCount // 6], (player[0], player[1]))
-        facing_up = True
-        facing_left = facing_right = facing_down = False
-    elif down:
-        screen.blit(walkDown[walkCount // 6], (player[0], player[1]))
-        facing_down = True
-        facing_left = facing_right = facing_up = False
-    else:
-        if facing_right:
-            screen.blit(charRight, (player[0], player[1]))
-        elif facing_left:
-            screen.blit(charLeft, (player[0], player[1]))
-        elif facing_up:
-            screen.blit(charUp, (player[0], player[1]))
-        elif facing_down:
-            screen.blit(charDown, (player[0], player[1]))
-        else:
-            screen.blit(charRight, (player[0], player[1]))
-
-    walkCount += 1
-
-    pygame.display.update()
-
-
-def redraw_window(character_info) -> None:
-    """
-    Create a 11x11 game board and refresh the display with an updated background
-
-    :postcondition: create a 11x11 game board and refresh the display with an updated background
-    """
-    screen.fill((0, 0, 0))
-    for row in range(GRID_SIZE):
-        for height in range(GRID_SIZE):
-            pygame.draw.rect(screen, (255, 255, 255),
-                             (row * CELL_SIZE, height * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
-
-    draw_character()  # draw the character
-    information_box(character_info, screen, player)
-
-    pygame.display.update()
-
-def game_quit() -> None:
+def game_quit():
     """
     Exit the game by closing the window or pressing the Escape key.
 
     :postcondition: exit the game after closing the window or pressing the Escape key
-    """
-    global game_run
+   """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            game_run = False
+            return False
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
-                game_run = False
+                return False
+    return True
 
-
-def movement() -> None:
+def movement(player, left, right, up, down):
     """
     Update the character position based on movement keys.
 
-    :postcondition: update character position based on movement keys
+    :param player: a pygame.Rect object representing the player
+    :param left: boolean indicating movement to the left
+    :param right: boolean indicating movement to the right
+    :param up: boolean indicating movement upwards
+    :param down: boolean indicating movement downwards
+    :precondition: left right up down should be booleans indicative of player direction
+    :postcondition: character position updated based on movement keys
+    :return: the walk count as an integer
     """
-    global left, right, up, down, player, walkCount
-
-    # movement keys
+    walk_count = 0
     if left and player[0] > SPEED - SPEED * 2:
         player.move_ip(-SPEED, 0)
-        walkCount += 1
+        walk_count += 1
     elif right and player[0] < SCREEN_WIDTH - PLAYER_WIDTH + SPEED:
         player.move_ip(SPEED, 0)
-        walkCount += 1
+        walk_count += 1
     elif up and player[1] > SPEED - SPEED:
         player.move_ip(0, -SPEED)
-        walkCount += 1
+        walk_count += 1
     elif down and player[1] < 925 - PLAYER_HEIGHT:
         player.move_ip(0, SPEED)
-        walkCount += 1
+        walk_count += 1
+    return walk_count
 
-
-def key_handle() -> None:
+def key_handle():
     """
-    Receive keyboard input based on the pressed key to update movement for the character.
+    Processes keyboard input to determine movement direction.
 
-
-    :postcondition: receive keyboard input and update the movement of the character based on the pressed key
+    :return: A tuple of booleans left, right, up, down indicating the movement direction
     """
-    global left, right, up, down
-
     keys = pygame.key.get_pressed()
+    left = keys[pygame.K_a] or keys[pygame.K_LEFT]
+    right = keys[pygame.K_d] or keys[pygame.K_RIGHT]
+    up = keys[pygame.K_w] or keys[pygame.K_UP]
+    down = keys[pygame.K_s] or keys[pygame.K_DOWN]
+    return left, right, up, down
 
-    if keys[pygame.K_a] or keys[pygame.K_LEFT]:
-        left = True
-        right = up = down = False
-    elif keys[pygame.K_d] or keys[pygame.K_RIGHT]:
-        right = True
-        left = up = down = False
-    elif keys[pygame.K_w] or keys[pygame.K_UP]:
-        up = True
-        left = right = down = False
-    elif keys[pygame.K_s] or keys[pygame.K_DOWN]:
-        down = True
-        left = right = up = False
-    else:
-        # reset all flags if no movement keys are pressed
-        left = right = up = down = False
-
-
-def is_player_on_target_square(target_row, target_column) -> bool:
+def is_player_on_target_square(player, target_row, target_column):
     """
-    Check if the player is on the target square.
+    Determines if the player has detected collisions.
 
-    :return: True if the player is on the target square, otherwise, False
+    :param player: a pygame.Rect object representing the player
+    :param target_row: the target row index as an integer
+    :param target_column: the target column index as an integer
+    :precondition: values provided are by pygame.Rect objects during live game
+    :postcondition: determines if the player has detected collisions
+    :return: true if the player is on the target square, otherwise False
     """
     target_x = target_row * CELL_SIZE
     target_y = target_column * CELL_SIZE
     return player.colliderect(pygame.Rect(target_x, target_y, CELL_SIZE, CELL_SIZE))
 
 def main():
-    """Drives the program"""
-    global game_run
-    global player
-    target_row = 5
-    target_column = 5
-    prompt_shown = False
+    """Drive the program"""
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    pygame.display.set_caption("Pikachu's Adventure!")
+    clock = pygame.time.Clock()
+
+    walk_left, walk_right, walk_up, walk_down, char_right, char_left, char_up, char_down = load_character_images()
+
+    walk_count = 0
+    facing_left = facing_right = facing_up = facing_down = False
+    game_run = True
 
     try:
         trainer_name = get_name(screen)
+        user_has_profile = user_has_file(trainer_name)
+        character_info = generate_character_info(trainer_name) if not user_has_profile else get_save_file(trainer_name)
     except ValueError as e:
-        print("Invalid Input: {}\nExiting by returning None".format(e), file=sys.stderr)
+        print("Invalid Input: {}\nExiting by returning None", file=sys.stderr)
         return None
 
-    user_has_profile = user_has_file(trainer_name)
-
-    character_info = {}
-    if not user_has_profile:
-        character_info = generate_character_info(trainer_name)
-    elif user_has_profile:
-        character_info = get_save_file(trainer_name)
-
-    bosses = initialize_bosses()
-    current_boss = bosses[character_info["bosses_beaten"]]
-    boss_fight = False
-
-    player = pygame.Rect(
-        (character_info['coordinates'][0], character_info['coordinates'][1], PLAYER_WIDTH, PLAYER_HEIGHT))
-
+    player = pygame.Rect(character_info['coordinates'][0], character_info['coordinates'][1], PLAYER_WIDTH, PLAYER_HEIGHT)
     show_intro_screen(character_info['name'], screen, clock)
+
+    target_row, target_column, prompt_shown = 5, 5, False
 
     while game_run:
         clock.tick(40)
 
-        game_quit()
-        key_handle()
-        movement()
-        information_box(character_info, screen, player)
+        game_run = game_quit()
+        left, right, up, down = key_handle()
+        walk_count += movement(player, left, right, up, down)
 
-        redraw_window(character_info)
+        character_args = (walk_count, facing_left, facing_right, facing_up, facing_down, left, right, up, down, walk_left, walk_right, walk_up, walk_down, char_right, char_left, char_up, char_down)
+        walk_count, facing_left, facing_right, facing_up, facing_down = redraw_window(character_info, screen, player, *character_args)
 
-        if is_player_on_target_square(target_row, target_column) and not prompt_shown:
-            user_choice = display_prompt(screen)
+        if is_player_on_target_square(player, target_row, target_column) and not prompt_shown:
+            display_prompt(screen)
             prompt_shown = True
-
-        elif not is_player_on_target_square(target_row, target_column):
+        elif not is_player_on_target_square(player, target_row, target_column):
             prompt_shown = False
-    pygame.quit()
 
+    pygame.quit()
 
 if __name__ == '__main__':
-    pygame.init()
     main()
-    pygame.quit()
---
+++ game_gui/drawing.py
@@ -0,0 +1,67 @@
+import pygame
+from pygame import Surface, Rect
+
+from constants import GRID_SIZE, CELL_SIZE
+from game_gui.information_box import information_box
+
+
+def redraw_window(character_info: dict, screen: Surface, player: Rect, *args):
+    """
+    Create a 11x11 game board and refresh the display with an updated background
+
+    :postcondition: create a 11x11 game board and refresh the display with an updated background
+    """
+    screen.fill((0, 0, 0))
+    for row in range(GRID_SIZE):
+        for height in range(GRID_SIZE):
+            pygame.draw.rect(screen, (255, 255, 255),
+                             (row * CELL_SIZE, height * CELL_SIZE, CELL_SIZE, CELL_SIZE), 1)
+
+    updated_args = draw_character(screen, player, *args)
+    information_box(character_info, screen, player)
+
+    pygame.display.update()
+    return updated_args
+
+def draw_character(screen, player, *args):
+    """
+    Draw the character on the screen based on the current state and direction it is facing.
+
+    :postcondition: character is drawn on the screen facing north, south, east, or west
+    """
+    walk_count, facing_left, facing_right, facing_up, facing_down, left, right, up, down, walk_left, walk_right, walk_up, walk_down, char_right, char_left, char_up, char_down = args
+    if walk_count >= 12:
+        walk_count = 0
+
+    if left:
+        screen.blit(walk_left[walk_count // 6], (player[0], player[1]))
+        facing_left = True
+        facing_right = facing_up = facing_down = False
+    elif right:
+        screen.blit(walk_right[walk_count // 6], (player[0], player[1]))
+        facing_right = True
+        facing_left = facing_up = facing_down = False
+    elif up:
+        screen.blit(walk_up[walk_count // 6], (player[0], player[1]))
+        facing_up = True
+        facing_left = facing_right = facing_down = False
+    elif down:
+        screen.blit(walk_down[walk_count // 6], (player[0], player[1]))
+        facing_down = True
+        facing_left = facing_right = facing_up = False
+    else:
+        if facing_right:
+            screen.blit(char_right, (player[0], player[1]))
+        elif facing_left:
+            screen.blit(char_left, (player[0], player[1]))
+        elif facing_up:
+            screen.blit(char_up, (player[0], player[1]))
+        elif facing_down:
+            screen.blit(char_down, (player[0], player[1]))
+        else:
+            screen.blit(char_right, (player[0], player[1]))
+
+    walk_count += 1
+
+    pygame.display.update()
+    return walk_count, facing_left, facing_right, facing_up, facing_down
