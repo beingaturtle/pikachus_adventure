644af04d4998bbcc10e424fa6f6a84dbc1ee71fe
Ian Chan <ianchan1108@hotmail.com>
2023-12-07 17:42:14 -0800

    Update docstring

--- game_gui/is_player_on_target_square.py
+++ game_gui/is_player_on_target_square.py
@@ -1,11 +1,12 @@
 import pygame
 from constants import CELL_SIZE
 
+
 def is_player_on_target_square(player, target_row, target_column):
     """
     Determines if the player has detected collisions.
 
-    :param player: a pygame.Rect object representing the player
+    :param player: a pygame object representing the player
     :param target_row: the target row index as an integer
     :param target_column: the target column index as an integer
     :precondition: values provided are by pygame.Rect objects during live game
