78fdaea542b75822894fb41ec93c6c48a0054f7d
Ian Chan <ianchan1108@hotmail.com>
2023-12-07 17:42:59 -0800

    Update docstring

--- game_gui/information_box.py
+++ game_gui/information_box.py
@@ -3,6 +3,7 @@ from pygame import Surface, Rect
 from utils.display_pikachu_stats import display_pikachu_stats
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 
+
 def information_box(character_status: dict, screen: Surface, player: Rect) -> None:
     """
     Displays an informative box at the bottom of the screen to show Pikachu's status.
@@ -11,6 +12,8 @@ def information_box(character_status: dict, screen: Surface, player: Rect) -> No
     :param screen: pygame screen object where the name will be inputted and displayed
     :param player: pygame player object representing the character location
     :precondition: character_status must be a non-empty dictionary that represents Pikachu's status
+    :precondition: screen must be a pygame screen object
+    :precondition: player must be a pygame player object representing the character location
     :postcondition: display an informative box at the bottom of the screen to show Pikachu's status
     """
     pygame.draw.rect(screen, (255, 255, 204), (0, 925, SCREEN_WIDTH, SCREEN_HEIGHT - 925))
