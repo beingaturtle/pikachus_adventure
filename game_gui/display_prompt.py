08a4a1663be30c3d4628cf2bdbb4bc64c9dffc68
Edro <edrogonzales@hotmail.com>
2023-12-09 13:14:36 -0800

    Refactor for modularity display_prompt

--- game_gui/display_prompt.py
+++ game_gui/display_prompt.py
@@ -1,18 +1,19 @@
 import sys
 import pygame
+from pygame import Surface
 
 
-def display_prompt(pygame_screen, state_status) -> str:
+def display_prompt(pygame_screen: Surface, message: str) -> str:
     """
-    Display a prompt with choices and return the user's selection.
+    Display a prompt for saving the current state of character.
 
     :param pygame_screen: The pygame screen object
-    :param state_status: The pygame screen object
+    :param message: string message to display
     :return: The user's selection as a string
     """
     font = pygame.font.Font(None, 36)
-    choices = f"{state_status}: Press 1 for Option A, 2 for Option B"
-    text = font.render(choices, True, (255, 255, 255))
+    text = font.render(message,
+                       True, (255, 255, 255))
     pygame_screen.blit(text, (100, 100))
     pygame.display.update()
 
@@ -20,9 +21,12 @@ def display_prompt(pygame_screen, state_status) -> str:
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_1:
-                    return '1'
+                    return "1"
                 elif event.key == pygame.K_2:
                     pygame_screen.fill((0, 0, 0))
                     pygame.display.flip()
                     return '2'
 
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
