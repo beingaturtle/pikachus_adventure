03a50f52d72ac02d41de8877d7f6672398a6b858
Ian Chan <ianchan1108@hotmail.com>
2023-12-09 15:36:33 -0800

    Add boss type and has_enough_keys

--- utils/initialize_bosses.py
+++ utils/initialize_bosses.py
@@ -1,6 +1,7 @@
-def has_enough_keys(character_info, boss_info):
-    if character_info["keys"] >= boss_info["required_key"]:
+def has_enough_keys(character_info, current_boss):
+    if character_info["keys"] >= current_boss["keys_required"]:
         return True
+
 
 def initialize_bosses():
     """
@@ -10,6 +11,7 @@ def initialize_bosses():
     :return: tuple of bosses and their information
     """
     first_boss = {
+        "enemy_type": "boss",
         "name": "Trainer Edro Gonzales",
         "health": 100,
         "coordinates": [
@@ -20,9 +22,11 @@ def initialize_bosses():
         "attack_power": 10,
         "agility": 10,
         "keys_required": 3,
+        "experience_award": 500
     }
 
     second_boss = {
+        "enemy_type": "boss",
         "name": "Trainer Ian Chan",
         "health": 100,
         "coordinates": [
@@ -33,9 +37,11 @@ def initialize_bosses():
         "attack_power": 20,
         "agility": 20,
         "keys_required": 6,
+        "experience_award": 500
     }
 
     third_boss = {
+        "enemy_type": "boss",
         "name": "Trainer Richard M",
         "health": 100,
         "coordinates": [
@@ -46,9 +52,11 @@ def initialize_bosses():
         "attack_power": 30,
         "agility": 30,
         "keys_required": 9,
+        "experience_award": 500
     }
 
     fourth_boss = {
+        "enemy_type": "boss",
         "name": "Trainer Chris Thompson",
         "health": 100,
         "coordinates": [
@@ -59,6 +67,7 @@ def initialize_bosses():
         "attack_power": 40,
         "agility": 40,
         "keys_required": 9,
+        "experience_award": 500
     }
 
     return first_boss, second_boss, third_boss, fourth_boss
