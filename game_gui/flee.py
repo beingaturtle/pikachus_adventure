7a4b02e2bc48d43ef921478934aa2e6ff93f724a
Ian Chan <ianchan1108@hotmail.com>
2023-12-09 11:46:45 -0800

    Update flee functionality

--- game_gui/flee.py
+++ game_gui/flee.py
@@ -4,13 +4,12 @@ ADD A DOCSTRING
 import pygame
 
 
-def flee(screen, player) -> None:
+def flee(screen) -> None:
     font = pygame.font.Font(None, 36)
     text = font.render("You ran away safely!", True, (255, 255, 255))
     screen.blit(text, (100, 100))
     pygame.display.update()
     pygame.time.delay(2000)
-    player.left -= 10
 
 
 def main():
