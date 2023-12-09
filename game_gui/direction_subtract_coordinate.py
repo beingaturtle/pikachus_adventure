60530333f52689b46122a3038cd203672e8b0e49
Ian Chan <ianchan1108@hotmail.com>
2023-12-09 11:46:27 -0800

    Update naming and functionality

--- game_gui/direction_subtract_coordinate.py
+++ game_gui/direction_subtract_coordinate.py
@@ -1,30 +1,30 @@
 import pygame
 
 
-def character_direction_facing(player, facing_left, facing_right, facing_up, facing_down):
+def direction_subtract_coordinate(player, direction):
     """
     Subtract the coordinate of the player based on the direction it is facing by 10 pixels.
 
-    :param drawn_character: a pygame object draws the player
     :param player: a pygame object representing the player
+    :param direction: a string representing the direction the player is facing
     :precondition: player must be a pygame object representing the player
     :precondition: drawn_character must be a pygame object that draws the player
     :postcondition: player's coordinate is subtracted by 10 pixels based on the direction it is facing
     >>> character = pygame.Rect(0, 0, 10, 10)
-    >>> character_direction_facing(character, True, False, False)
+    >>> direction_subtract_coordinate(character, 'left')
     >>> character
-    <rect(-10, 0, 10, 10)>
+    <rect(10, 0, 10, 10)>
 
     >>> character = pygame.Rect(0, 10, 15, 15)
-    >>> character_direction_facing(character, False, False, True)
+    >>> direction_subtract_coordinate(character, 'up')
     >>> character
-    <rect(0, 0, 15, 15)>
+    <rect(0, 20, 15, 15)>
     """
-    if facing_left:
-        player.left -= 10
-    elif facing_right:
-        player.right += 10
-    elif facing_up:
-        player.top -= 10
-    elif facing_down:
-        player.bottom += 10
+    if direction == 'left':
+        player.left += 10
+    elif direction == 'right':
+        player.right -= 10
+    elif direction == 'up':
+        player.top += 10
+    elif direction == 'down':
+        player.bottom -= 10
