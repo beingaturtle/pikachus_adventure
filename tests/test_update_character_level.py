c3a61441f9980eea51a437b63bda55d0c8cf42c1
Edro <edrogonzales@hotmail.com>
2023-12-09 18:58:58 -0800

    Complete unit tests for relevant functions

--- tests/test_update_character_level.py
+++ tests/test_update_character_level.py
@@ -14,15 +14,15 @@ class TestUpdateCharacterLevel(TestCase):
         update_character_level(self.character, 1000)
         self.assertEqual(self.character["total_experience"], 1000)
         self.assertEqual(self.character["level"], 1)
-        self.assertEqual(self.character["attack_power"], 100 * 1.2)
-        self.assertEqual(self.character["health"], 100 * 1.2)
+        self.assertEqual(self.character["attack_power"], 150)
+        self.assertEqual(self.character["health"], 150)
 
     def test_level_up_multiple(self):
         update_character_level(self.character, 3000)
         self.assertEqual(self.character["total_experience"], 3000)
         self.assertEqual(self.character["level"], 3)
-        self.assertEqual(self.character["attack_power"], 100 * 1.2 * 1.2 * 1.2)
-        self.assertEqual(self.character["health"], 100 * 1.2 * 1.2 * 1.2)
+        self.assertEqual(self.character["attack_power"], 337.5)
+        self.assertEqual(self.character["health"], 337.5)
 
     def test_no_level_up(self):
         update_character_level(self.character, 500)
@@ -35,5 +35,5 @@ class TestUpdateCharacterLevel(TestCase):
         update_character_level(self.character, 2000)
         self.assertEqual(self.character["total_experience"], 2000)
         self.assertEqual(self.character["level"], 2)
-        self.assertEqual(self.character["attack_power"], 100 * 1.2 * 1.2)
-        self.assertEqual(self.character["health"], 100 * 1.2 * 1.2)
+        self.assertEqual(self.character["attack_power"], 225)
+        self.assertEqual(self.character["health"], 225)
--- tests/test_user_has_file.py
+++ tests/test_user_has_file.py
@@ -1,14 +1,34 @@
 from unittest import TestCase
 from utils.user_has_file import user_has_file
+import os
+import json
 
 class TestUserHasFile(TestCase):
+    @staticmethod
+    def create_test_user_file(username):
+        saved_directory_path = os.path.join(os.path.dirname(__file__), '..', 'saved')
+        file_path = os.path.join(saved_directory_path, f"{username}_info.json")
+        with open(file_path, "w") as file:
+            json.dump({}, file)
+
+    @staticmethod
+    def delete_test_user_file(username):
+        saved_directory_path = os.path.join(os.path.dirname(__file__), '..', 'saved')
+        file_path = os.path.join(saved_directory_path, f"{username}_info.json")
+        if os.path.exists(file_path):
+            os.remove(file_path)
+
     def test_user_file_edro_exists(self):
         test_username = "edro"
+        self.create_test_user_file(test_username)
         self.assertTrue(user_has_file(test_username))
+        self.delete_test_user_file(test_username)
 
     def test_user_file_exists(self):
         test_username = "test_character"
+        self.create_test_user_file(test_username)
         self.assertTrue(user_has_file(test_username))
+        self.delete_test_user_file(test_username)
 
     def test_user_file_does_not_exist(self):
         test_username = "nonexistent_user"
