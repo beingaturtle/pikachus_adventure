da37aeacdf53e0f5e0ac4fe9cce14f3d733a7f54
Ian Chan <ianchan1108@hotmail.com>
2023-12-09 22:22:30 -0800

    Update docstring

--- utils/display_pikachu_stats.py
+++ utils/display_pikachu_stats.py
@@ -1,13 +1,21 @@
+"""
+Pikachu's Adventure
+Edro Gonzales A01257468
+Ian Chan A00910012
+"""
 import pygame
+from pygame import Surface, Rect
 
 
-def display_pikachu_stats(pygame_screen, player, character_info: dict) -> None:
+def display_pikachu_stats(pygame_screen: Surface, player: Rect, character_info: dict) -> None:
     """
     Display Pikachu's current stats.
 
     :param pygame_screen: pygame screen object where the name will be inputted and displayed
     :param player: pygame rect object where the object represents Pikachu's coordinates and size
     :param character_info: a non-empty dictionary
+    :precondition: pygame_screen must be a pygame screen object representing the game screen
+    :precondition: player must be a pygame rect object representing Pikachu's coordinates and size
     :precondition: character_info must be a non-empty dictionary containing the user character information
     :postcondition: display Pikachu's current stats
     """
