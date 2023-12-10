1ce56c7ae01e6f3e58977bd4da3cc8fffd150580
Ian Chan <ianchan1108@hotmail.com>
2023-12-09 22:36:06 -0800

    Update docstring

--- utils/state_machine.py
+++ utils/state_machine.py
@@ -1,32 +1,39 @@
+"""
+Pikachu's Adventure
+Edro Gonzales A01257468
+Ian Chan A00910012
+"""
 import pygame
 import random
 from pygame import Rect
 from constants import CELL_SIZE
 
-def is_collision(player, area):
-    """
-    Decides during gameplay if the user is experiencing a collision.
 
-    :param player: player is a pygame.Rect object and provides information on character in game
+def is_collision(player: Rect, area: tuple) -> bool:
+    """
+    Checks if the user is experiencing a collision during gameplay.
+
+    :param player: player is a pygame object and provides information on character in game
     :param area: area is a tuple representing the location they are at
     :precondition: function being called during gameplay
-    :precondition: player must be a pygame.Rect object
+    :precondition: player must be a pygame object
     :precondition: area is a tuple with x and y locations
-    :postcondition: checks if player is within given rectangle and returns a boolean
-    :return: True if player is within given rectangle and then False otherwise
-
-    True
+    :postcondition: checks if player is in given area and is experiencing a collision
+    :return: a boolean value True representing if player is in given area, otherwise False
     """
     return player.colliderect(pygame.Rect(area[0], area[1], CELL_SIZE, CELL_SIZE))
+
 
 def state_machine(player: Rect, character_info: dict, boss_info: tuple) -> str:
     """
     Determines the state of the game.
 
-    :param player: pygame.Rect object
+    :param player: a pygame object that represents the player
     :param character_info: dictionary object containing character information
     :param boss_info: tuple containing all boss information
     :precondition: player must be a pygame.Rect object that represents the info of the current character
+    :precondition: character_info must be a dictionary object containing character information
+    :precondition: boss_info must be a tuple containing all boss information
     :postcondition: boss_info must be a tuple containing all boss information
     :postcondition: if player is moving around and not in any state return live_play
     :postcondition: if player is on a boss location then return boss state
@@ -41,7 +48,7 @@ def state_machine(player: Rect, character_info: dict, boss_info: tuple) -> str:
 
     next_boss = character_info["bosses_beaten"]
     current_boss = boss_info[next_boss]
-    boss_location = current_boss['coordinates']
+    boss_location = tuple(current_boss.get('coordinates', (0, 0)))
 
     areas = {
         "save_state": (410, 25),
