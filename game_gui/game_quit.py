1538a5e8c894ef768c204abc95ce311b05b7da34
Ian Chan <ianchan1108@hotmail.com>
2023-12-09 21:46:57 -0800

    Update docstring

--- game_gui/game_quit.py
+++ game_gui/game_quit.py
@@ -1,10 +1,17 @@
+"""
+Pikachu's Adventure
+Edro Gonzales A01257468
+Ian Chan A00910012
+"""
 import pygame
 
-def game_quit():
+
+def game_quit() -> bool:
     """
     Exit the game by closing the window or pressing the Escape key.
 
     :postcondition: exit the game after closing the window or pressing the Escape key
+    :return: False if the window is closed or the Escape key is pressed, True otherwise
    """
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
