a8a93d75e2f1a75b33bb03cbebe1961a69841c3e
Ian Chan <ianchan1108@hotmail.com>
2023-12-09 18:58:06 -0800

    Add messages for each boss

--- game_gui/display_boss_prompt.py
+++ game_gui/display_boss_prompt.py
@@ -3,16 +3,34 @@ import pygame
 from pygame import Surface
 
 
-def display_boss_prompt(pygame_screen: Surface) -> str:
+def display_boss_prompt(pygame_screen: Surface, current_boss) -> str:
     """
-    Display a prompt for saving the current state of character.
+    Displays a message from the boss and prompts the user to fight or not.
 
-    :param pygame_screen: The pygame screen object
-    :return: The user's selection as a string
+    :param pygame_screen: a pygame screen object
+    :param current_boss: the current boss is the boss that the user is currently facing
+    :precondition: pygame_screen must be a pygame screen object
+    :precondition: current_boss must be a dictionary containing information about the current boss
+    :postcondition: displays a message from the boss and prompts the user to fight or not
+    :return: a string representing the user's choice
     """
     font = pygame.font.Font(None, 36)
-    text = font.render("You dare to challenge me? Press 1 for Yes, 2 for No", True, (255, 255, 255))
-    pygame_screen.blit(text, (100, 100))
+
+    if current_boss['name'] == "Trainer Edro Gonzales":
+        text = font.render("Name's Pedro without the P. You dare to fight? Press 1 for Yes, 2 for No",
+                           True, (255, 255, 255))
+    elif current_boss['name'] == "Trainer Ian Chan":
+        text = font.render("It is I, Ian. You will lose if you try to fight me. Press 1 for Yes, 2 for No",
+                           True, (255, 255, 255))
+    elif current_boss['name'] == "Trainer Richard M":
+        text = font.render("I am Richard and I will destroy you. Press 1 for Yes, 2 for No", True,
+                           (255, 255, 255))
+    elif current_boss['name'] == "Trainer Chris Thompson":
+        text = font.render("I'm Chris, let me make a cup of tea before I quiz you. Press 1 for Yes, 2 for No",
+                           True, (255, 255, 255))
+    else:
+        text = font.render("You have defeated all the bosses. You win!", True, (255, 255, 255))
+    pygame_screen.blit(text, (20, 100))
     pygame.display.update()
 
     while True:
